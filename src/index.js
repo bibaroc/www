aea98bdaceab97b47c85ae4c6c94902b6f1965b7
vladyslav <vladyslav@mangrovia.solutions>
2020-06-05 00:55:01 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import './style';
+import { Router, route } from 'preact-router';
+import { authKey } from './const/storage';
+import * as pages from './const/pages';
 import { Component } from 'preact';
 
+import Home from './routes/home';
+import Signin from './routes/signin';
+
+import './style';
+
 export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello, World!</h1>
-			</div>
-		);
-	}
+    handleRoute = async e => {
+        this.setState({
+            currentUrl: e.url
+        });
+    }
+    constructor() {
+        super();
+        this.state = { currentUrl: '' };
+    }
+    componentDidMount() {
+        const userKey = localStorage.getItem(authKey);
+        console.log(`userKey: ${userKey}`);
+        if (!userKey) {
+            route(pages.signin, true);
+        }
+    }
+    render() {
+        return (
+            <div id="app">
+                <Router onChange={this.handleRoute}>
+                    <Home path="{pages.home}" />
+                    <Signin path={pages.signin} />
+                    <Home default />
+                </Router>
+            </div>
+        );
+    }
 }
