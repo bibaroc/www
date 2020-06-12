039be0a6c6db7923496c2371bc81c55ea917bf3b
bibaroc <sulimovskyy.vladyslav@gmail.com>
2020-06-12 17:04:30 +0200

    little sign in page

--- src/routes/signin/index.js
+++ src/routes/signin/index.js
@@ -1,15 +1,77 @@
 import { Component } from 'preact';
+import {
+    Input,
+    InputAdornment,
+    IconButton,
+    InputLabel,
+    TextField,
+    Button,
+    Container,
+    Typography,
+    Avatar,
+} from '@material-ui/core';
+
+import { FormControl, FormControlLabel, Checkbox } from '@material-ui/core'
+
+import { Visibility, VisibilityOff, LockOutlined } from '@material-ui/icons';
+
 
 export default class Signin extends Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            showPassword: false,
+            username: null,
+            password: null
+        };
+    }
+    handleClickShowPassword = () => {
+        let showPassword = this.state.showPassword;
+        this.setState({ showPassword: !showPassword });
     }
     render() {
         return (
-            <div>
-                <h1>Signin page</h1>
-            </div>
+            <Container component="main" maxWidth="xs">
+                <Avatar>
+                    <LockOutlined />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type={this.state.showPassword ? "text" : "password"}
+                    autoComplete="current-password"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment onClick={this.handleClickShowPassword}>
+                                {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                >
+                    Sign In
+                    </Button>
+            </Container>
         );
     }
 }
